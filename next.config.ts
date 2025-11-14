9f015206ccb79876cb9ee93c838b972b42efd1f8
Mazun Volodimir <mazunvolodimir777@gmail.com>
2025-11-14 11:25:57 +0200

    correct file next-config-ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,22 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactCompiler: true,
 
-  images: {
-	  remotePatterns: [
-	      { protocol: 'https', hostname: 'picsum.photos' }
-	    ]
+	images: {
+		domains: ['picsum.photos'],
+	},
+	async headers() {
+		return [
+			{
+				source: '/notes/filter/:slug', // маршрут сторінки
+				locale: false,
+				headers: [
+					{
+						key: 'Cache-Control', // Заголовок
+						value: 'public, max-age=300, must-revalidate', // кешуємо на 5 хв
+					},
+				],
+			},
+		]
 	}
 };
 
